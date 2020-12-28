adf8de6e4bd136657c4a33205cf0a5b8a8204783
Petar Hamamdzhiev <hamamdzhiev@hotmail.com>
2020-12-28 10:49:52 +0200

    change what is selected

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -7,7 +7,7 @@ class User
 	
 	public function __construct($con, $user) {
 		$this->con = $con;
-		$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$user'");
+		$user_details_query = mysqli_query($con, "SELECT first_name, last_namegit FROM users WHERE username='$user'");
 		$this->user = mysqli_fetch_array($user_details_query);
 	}
 	public function getUsername() {
